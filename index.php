d289e6ee24c6b5ca71652e52e338fc343a7bc977
Naufal  Ardhani <naufal201080@gmail.com>
2024-03-07 17:14:55 +0700

    commit ini menambah file style.css

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Ini latihan kasus 2</title>
 </head>
 <body>
@@ -12,7 +13,7 @@
         <li>Alamat: Politeknik</li>
         <li>NIM: 4342301061</li>
     </ul>
-
+    
     <h2>makan yuk</h2>
 </body>
 </html>
